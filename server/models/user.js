4e033749d364b5f484ba802c62dfec2c5da64af7
gerawerk <gerawerkzewudu2121@gmail.com>
2025-07-11 12:43:19 +0300

    model id change

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   _id: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     required: true,
   },
   name: {
